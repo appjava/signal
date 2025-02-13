7d5186d70993a200b9182d7b1cabb40137ed5195
javaair <appjavadev2019@gmail.com>
2025-02-12 23:08:11 -0500

    add time

--- meTV/meFunctions.js
+++ meTV/meFunctions.js
@@ -200,5 +200,17 @@ function playCH(ch){
 
 }
 
+// Show TIME
+document.getElementById("time").innerHTML = showTime();
 
+    function showTime() {
+    var d = new Date(),
+        minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+        hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+        ampm = d.getHours() >= 12 ? 'pm' : 'am',
+        months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+        days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+        //return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+        return days[d.getDay()]+' '+d.getDate()+' '+months[d.getMonth()]+' '+hours+':'+minutes;
+    }
 
