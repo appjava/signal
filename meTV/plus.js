12bae83037aa1e280184e2c938f6fbf4462b4480
echos <appjavadev2019@gmail.com>
2025-02-17 18:58:56 -0500

    add chs to meTV

--- meTV/plus.js
+++ meTV/plus.js
@@ -2,7 +2,7 @@
 
 var frame = document.querySelector("iframe");
 var meframe = document.getElementById('meframe');
-meframe.src = "";
+meframe.src = "https://embed.sdfgnksbounce.com/embed/fx.html";
 //meframe.src = "https://youtube.com/embed/pKmSdY56VtY";
 let id;
 
