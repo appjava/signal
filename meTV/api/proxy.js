7f21a9acfefcf575449f7b70ef26ed467b034aeb
javaair <appjavadev2019@gmail.com>
2025-03-18 08:50:32 -0500

    change refer proxy

--- meTV/api/proxy.js
+++ meTV/api/proxy.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
         const response = await fetch(url, {
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Referer': 'https://metv.vercel.app/' // Cambia esto a la URL que el servidor espera
             }
         });
 
