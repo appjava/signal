fd717c35394f7c61d9124aaaee0b9f82bdc282d8
javaair <appjavadev2019@gmail.com>
2025-04-04 16:41:38 -0500

    change iframe

--- meTV/plus.js
+++ meTV/plus.js
@@ -13,7 +13,7 @@ var frame = document.querySelector("iframe");
 var meframe = document.getElementById('meframe');
 
 if (meframe){
-  meframe.src = "https://embed.sdfgnksbounce.com/embed2/tnt.html";  
+  meframe.src = "https://embed.sdfgnksbounce.com/embed/tnt.html";  
 }
 
 
